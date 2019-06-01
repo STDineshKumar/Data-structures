6a315de1ddf202fa74164f762a7636542ab48b4f
sriram161 <k.sriramsagar@gmail.com>
2019-06-01 12:41:36 -0400

    Merge sort completed.

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -1,20 +1,9 @@
-def merge_sort(list_):
-    if len(list_) > 1:
-        mid = len(list_) // 2
-        left_list = list_[:mid]
-        right_list = list_[mid:]
-        l = merge_sort(left_list)
-        r = merge_sort(right_list)
-        return merge(l, r)
-    return list_
-    
-
 def merge(list1, list2):
     merge_list = []
     i=0
     j=0
-    l1, l2 = len(list1)-1, len(list2)-1
-    while(i+j <= l1+l2):
+    l1, l2 = len(list1), len(list2)
+    while(i < l1 and j < l2):
             if list1[i] < list2[j]:
                     merge_list.append(list1[i])
                     i+=1
@@ -26,15 +15,28 @@ def merge(list1, list2):
                     merge_list.append(list2[j])
                     i+=1
                     j+=1
-    while(i <= l1):
+    while(i < l1):
             merge_list.append(list1[i])
             i+=1
-    while(j <= l2):
-            merge_list.append(list2[j])
-    return merge_list 
 
+    while(j < l2):
+            merge_list.append(list2[j])
+            j+=1
+    return merge_list
+
+def merge_sort(list_):
+    if len(list_) > 1:
+        mid = len(list_) // 2
+        left_list = list_[:mid]
+        right_list = list_[mid:]
+        l = merge_sort(left_list)
+        r = merge_sort(right_list)
+        return merge(l, r)
+    return list_
+    
 
 if __name__ == '__main__':
         a = [5, 4, 3, 1, 2]
+        print(a)
         b = merge_sort(a)
         print(b)
