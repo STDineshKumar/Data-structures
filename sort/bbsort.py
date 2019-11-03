45087b4cda9038c0cb492a403fef7cf9d453d293
sriram161 <k.sriramsagar@gmail.com>
2019-11-02 20:23:18 -0400

    bb commented.

--- sort/bbsort.py
+++ sort/bbsort.py
@@ -1,4 +1,21 @@
 def bbsort(a, j=0):
+    """ Recursive bubble sort.
+        Logic :
+        -------
+            Compare adjacent items in a list and swap items if lower index item is greater than the higher index item.
+            Note*** Highest value item is moved to higest index.
+            Sub-array n-1 elemens and repeate the compare process until sub-array lenght becomes zero.
+        NOTE :
+        ------
+            This function uses in-place swap logic.
+        Space Complexity:
+        -----------------
+            O(N)
+
+        Time Complexity:
+        ----------------
+            O(N^2) = N*(N+1)/2
+    """
     l = len(a)
     for i in range(l-j-1):
         if a[i] > a[i+1]:
@@ -9,6 +26,8 @@ def bbsort(a, j=0):
     
 
 def bbsort2(a):
+    """ Iterative bubble sort.
+    """
     l = len(a)
     j =  0
     while ( l-j > 0):
@@ -20,4 +39,5 @@ def bbsort2(a):
 if __name__ == '__main__':
     a = [4,5,3,2,1]
     bbsort(a)
+    bbsort2(a)
     print(a)
